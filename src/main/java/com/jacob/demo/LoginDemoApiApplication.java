dc0cde9cdcf7c60c90acae63257a54a46cbfddc0
Jacob2071 <45714347+Jacob2071@users.noreply.github.com>
2020-05-03 11:22:32 -0800

    Initial commit

---
+++ src/main/java/com/jacob/demo/LoginDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jacob.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDemoApiApplication.class, args);
+	}
+
+}
